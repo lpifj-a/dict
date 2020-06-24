2d3c0c391617d9d2f4cceae5732353a2cf16fd1a
unknown <suket_1111@icloud.com>
2020-06-24 20:21:46 +0900

    final

--- main3.py
+++ main3.py
@@ -50,6 +50,26 @@ def change(str):
     s = ''.join(b)
     return s
 
+def scrape(q):
+    url = "https://dictionary.goo.ne.jp/word/"+q
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+    #print(soup.prettify())　#htmlの表示
+    strings = []
+    items = soup.find_all("meta")
+    for item in items:
+        inf = item.get("content")
+        if inf is None:
+            continue
+        if "noindex" in inf:
+            return 0
+        if "意味" in inf:
+            strings.append(inf)
+    #print(strings[0])
+    ans = []
+    ans = strings[0].split("。")
+    return ans[2]
+
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -70,9 +90,19 @@ def callback():
 # MessageEvent
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    q1 = event.message.text
+    q3 = 0
+    count = 0
+    while(q3==0):
+        q2 = change(q1)
+        q3 = scrape(q2)
+        count += 1
+        if(count > 100):
+            q3 = "みつかりませんでした。"
+            break
 	line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=change(event.message.text))
+        TextSendMessage(text=q3)
      )
 
 if __name__ == "__main__":
