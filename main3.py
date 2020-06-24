2754bf022d08e018101a3e27080f7e207cdcbb1a
unknown <suket_1111@icloud.com>
2020-06-24 20:15:49 +0900

    final

--- main3.py
+++ main3.py
@@ -72,7 +72,7 @@ def callback():
 def handle_message(event):
 	line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(change(text=event.message.text))
+        TextSendMessage(text=change(event.message.text))
      )
 
 if __name__ == "__main__":
