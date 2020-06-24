7496ac9a97701e9035cdbafb9bb5a646f1de507f
unknown <suket_1111@icloud.com>
2020-06-24 20:09:38 +0900

    final

--- main3.py
+++ main3.py
@@ -41,15 +41,14 @@ def callback():
 
     return 'OK'
 
-
 # MessageEvent
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 	line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)
+        TextSendMessage(text='「' + event.message.text + '」って何？')
      )
 
-if __name__ == "__main3__":
+if __name__ == "__main__":
     port = int(os.getenv("PORT"))
     app.run(host="0.0.0.0", port=port)
