6e7f78ec61e4f36486fee7ea1d1ab12dff28cf55
unknown <suket_1111@icloud.com>
2020-06-25 16:42:45 +0900

    button

--- main3.py
+++ main3.py
@@ -90,18 +90,11 @@ def callback():
 # MessageEvent
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    q1 = event.message.txt
-    if q1 == '辞書':
-        buttons_template = ButtonsTemplate(
-            title='My buttons sample', text='Hello, my buttons', actions=[
-                URIAction(label='Go to line.me', uri='https://line.me'),
-                PostbackAction(label='ping', data='ping'),
-                PostbackAction(label='ping with text', data='ping', text='ping'),
-                MessageAction(label='Translate Rice', text='米')
-            ])
-        template_message = TemplateSendMessage(alt_text='Buttons alt text', template=buttons_template)
-        line_bot_api.reply_message(event.reply_token, template_message)
+    text = event.message.txt
+    if text == '辞書':
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text="hello"))
     else:
+        q1 = text
         q3 = 0
         count = 0
         while(q3==0):
