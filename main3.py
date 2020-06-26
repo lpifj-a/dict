f8a896351e4602b621e323e84cb1b4f2d0871cc1
unknown <suket_1111@icloud.com>
2020-06-27 00:57:19 +0900

    quik replay

--- main3.py
+++ main3.py
@@ -143,24 +143,7 @@ def handle_message(event):
                 quick_reply=QuickReply(
                     items=[
                         QuickReplyButton(
-                            action=PostbackAction(label="label1", data="data1")
-                        ),
-                        QuickReplyButton(
-                            action=MessageAction(label="label2", text="text2")
-                        ),
-                        QuickReplyButton(
-                            action=DatetimePickerAction(label="label3",
-                                                        data="data3",
-                                                        mode="date")
-                        ),
-                        QuickReplyButton(
-                            action=CameraAction(label="label4")
-                        ),
-                        QuickReplyButton(
-                            action=CameraRollAction(label="label5")
-                        ),
-                        QuickReplyButton(
-                            action=LocationAction(label="label6")
+                            action=MessageAction(label="？", text=q2+"(https://dictionary.goo.ne.jp/word/"+q2+")")
                         ),
                     ])))
 
